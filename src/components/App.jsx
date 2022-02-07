83697c1da09c5ac1788e73e7ce82c7a8fe98c85d
Alexey Dolgiy <alexey.dolgiy@gmail.com>
2022-02-07 21:30:39 +0200

    added component Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,39 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import Searchbar from "./Searchbar/Searchbar";
+
+export class App extends Component {
+  componentDidMount() {
+    fetch('https://pixabay.com/api/?q=cat&page=1&key=24541391-b479c34a264a17829baf6aba8&image_type=photo&orientation=horizontal&per_page=12')
+  }
+
+  render() {
+    return (
+      <Searchbar />
+
+    )
+  };
 };
+
+
+
+
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         textTransform: 'uppercase',
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,24 @@
+import react from "react";
+import propTypes from "prop-types";
+
+const Searchbar = () => {
+    return (
+        <header className="searchbar">
+            <form className="form">
+                <button type="submit" className="button">
+                    <span className="button-label">Search</span>
+                </button>
+
+                <input
+                    className="input"
+                    type="text"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                />
+            </form>
+        </header>
+    )
+};
+
+export default Searchbar;
