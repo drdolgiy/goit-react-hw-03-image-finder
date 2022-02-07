ea29b8a0f654659fa1691ab49643fa3945c103b3
Alexey Dolgiy <alexey.dolgiy@gmail.com>
2022-02-08 01:26:49 +0200

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,25 +1,42 @@
+import { Component } from "react";
 import { ImSearch } from "react-icons/im";
 // import propTypes from "prop-types";
 
-const Searchbar = () => {
-    return (
-        <header className="searchbar">
-            <form className="form">
-                <button type="submit" className="button">
-                    <ImSearch style={{ marginRight: 8 }} />
-                    <span className="button-label">Search</span>
-                </button>
 
-                <input
-                    className="input"
-                    type="text"
-                    autoComplete="off"
-                    autoFocus
-                    placeholder="Search images and photos"
-                />
-            </form>
-        </header>
-    )
+export default class Searchbar extends Component {
+
+    state = {
+        imageName: '',
+    };
+
+    handleNameChange = event => {
+        this.setState({ imageName: event.currentTarget.value.toLowerCase() })
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+    }
+
+
+    render() {
+        return (
+            <header className="searchbar">
+                <form onSubmit={this.handleSubmit} className="form">
+                    <button type="submit" className="button">
+                        <ImSearch style={{ marginRight: 8 }} />
+                        <span className="button-label">Search</span>
+                    </button>
+
+                    <input
+                        className="input"
+                        onChange={this.handleNameChange}
+                        type="text"
+                        autoComplete="off"
+                        autoFocus
+                        placeholder="Search images and photos"
+                    />
+                </form>
+            </header>
+        )
+    }
 };
-
-export default Searchbar;
